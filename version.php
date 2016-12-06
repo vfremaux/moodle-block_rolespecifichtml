fbb4555b5e69161a5287acdddbfbcaa301b00eb9
valery <valery.fremaux@gmail.com>
2016-12-07 00:53:55 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -15,23 +15,22 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Version details.
  *
  * @package    block_rolespecifichtml
  * @category   blocks
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @author     valery fremaux <valery.fremaux@gmail.com>
+ * @copyright  2012 onwards valery fremaux (valery.fremaux@gmail.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016083100;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2016051900;        // Requires this Moodle version
+$plugin->requires  = 2014110400;        // Requires this Moodle version
 $plugin->component = 'block_rolespecifichtml';  // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.1.0 (build 2016083100)';
+$plugin->release = '2.8.0 (build 2016083100)'; 
 
-// Non Moodle fields
-// This fields will help overmanagement code builders without forcing upgrade to play
-$plugin->codeversion = 2016083100.00;
-$plugin->codeincrement = 0;
+// Non moodle attributes.
+$plugin->codeincrement = '2.8.0000';
